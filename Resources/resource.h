3e04d132312ca7dac8ebdb017a56a2f39e39c504
Josh Patterson <josh@ntdls.com>
2018-04-09 15:51:01 -0400

    no message

---
+++ Resources/resource.h
@@ -0,0 +1,81 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DiskMark.rc
+//
+#define IDS_TITLECAPTION                1
+#define IDS_FILEVERSION                 2
+#define IDS_APPCOPYRIGHT                3
+#define IDS_REGISTRYKEY                 4
+#define IDC_HANDCURSOR                  100
+#define IDI_MAINICON                    101
+#define IDI_MAIN                        101
+#define IDD_CONFIG                      102
+#define IDD_MAIN                        105
+#define IDD_NETLOGO1                    146
+#define IDD_NETLOGO                     146
+#define IDC_TEXTBOX                     1000
+#define IDC_NETLINK                     1000
+#define IDC_OK                          1001
+#define IDC_CMDCANCEL                   1001
+#define IDC_CANCEL                      1002
+#define IDC_STATICTEXT                  1003
+#define IDC_CMDOK                       1003
+#define IDC_ABOUTICON                   1004
+#define IDC_CMDOK2                      1004
+#define IDC_CMDHELP                     1004
+#define IDC_APPNAME                     1005
+#define IDC_ALLOWCACHE                  1006
+#define IDC_APPVERSION                  1006
+#define IDC_IOS                      1007
+#define IDC_APPCOPYRIGHT                1007
+#define IDC_ITERATIONS                        1008
+#define IDC_OKBUTTON                    1008
+#define IDC_CHUNKSIZE                  1009
+#define IDC_BUILDTIMEDATE               1009
+#define IDC_TOTALWORKINGSET             1010
+#define IDC_BYTESPERITERATION                  1010
+#define IDC_TOTALDISKTIME               1011
+#define IDC_DISKTIME                    1011
+#define IDC_DISKTIMEWRITE               1011
+#define IDC_TOTALMBWRITTEN              1012
+#define IDC_DISKTIMEREAD                1012
+#define IDC_LASTMBPERS                  1013
+#define IDC_LAST                        1013
+#define IDC_LASTWRITE                   1013
+#define IDC_AVERAGEMBPERS               1014
+#define IDC_AVG                         1014
+#define IDC_AVGWRITE                    1014
+#define IDC_BESTWORSEMBPERS             1015
+#define IDC_MAX                         1015
+#define IDC_MAXWRITE                    1015
+#define IDC_PROGRESS1                   1016
+#define IDC_DSKPROGRESS                 1016
+#define IDC_BESTWORSEMBPERS2            1017
+#define IDC_MIN                         1017
+#define IDC_MINWRITE                    1017
+#define IDC_AVGREAD                     1018
+#define IDC_LASTREAD                    1019
+#define IDC_MINREAD                     1020
+#define IDC_DISKDRIVE                   1021
+#define IDC_ALLOWCACHING                1022
+#define IDC_MAXREAD                     1022
+#define ID_CMDMULTI                     1023
+#define IDC_GRAPH                       1024
+#define IDC_WRITEGRAPH                  1024
+#define IDC_GRAPHWRITE                  1024
+#define IDC_ONTOP                       1025
+#define IDC_GRAPH2                      1026
+#define IDC_READGRAPH                   1026
+#define IDC_GRAPHREAD                   1026
+#define IDC_TOTALDATASETSIZE                1028
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1028
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
